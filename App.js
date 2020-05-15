c8776ce86db632108d6eed73cd994bad4061a15e
michaldobiezynski <dobiezynski@gmail.com>
2020-05-15 10:05:03 +0100

    A better list: FlatList

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   Button,
   ScrollView,
+  FlatList,
 } from 'react-native';
 
 export default function App() {
@@ -17,7 +18,10 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals([
+      ...courseGoals,
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
   };
 
   return (
@@ -31,13 +35,13 @@ export default function App() {
         />
         <Button title='Add' onPress={addGoalHandler} />
       </View>
-      <ScrollView >
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
           </View>
-        ))}
-      </ScrollView>
+        )}></FlatList>
     </View>
   );
 }
