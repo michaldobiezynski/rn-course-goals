ef5f6adc263813fba399bd24374064e4ee89e9f6
michaldobiezynski <dobiezynski@gmail.com>
2020-05-16 10:14:23 +0100

    removing goals and modal

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const GoalItem = (props) => {
+const GoalItem = props => {
   return (
-    <View style={styles.listItem}>
-      <Text>{props.title}</Text>
-    </View>
+    <TouchableOpacity onPress={props.onDelete.bind(this, props.id)} >
+      <View style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
@@ -15,8 +17,8 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: '#ccc',
     borderColor: 'black',
-    borderWidth: 1,
-  },
+    borderWidth: 1
+  }
 });
 
 export default GoalItem;
